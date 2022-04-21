482910db30f46b759273d8c3e9a35eac77cf4101
Liberty Mtabvuri <leereal08@ymail.com>
2022-04-21 15:28:08 +0200

    Removed Frontend Git

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Chipamall</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
